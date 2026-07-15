0cc54839cb67cffad914631c61530c79ca194b60
Robson Mesquita <Robsonmes11@gmail.com>
2026-07-15 11:38:39 -0300

    "Alterações para criar usuarios no banco de dados, erros de devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,22 @@
 class User < ApplicationRecord
     has_one :student, dependent: :destroy
+    
+    devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, authentication_keys: [:cpf]
+
+    def email_required?
+        false
+    end
+    
+    def email_changed?
+        false
+    end
+
+    def will_save_change_to_email?
+        false
+    end
+    
+    enum :role, { admin: "admin", user: "user" }
 
     validates :cpf, presence: true, length: { is: 11 }, uniqueness: true, numericality: true
-    validates :password_digest, presence: true, length: { in: 6..10 }, numericality: true
-
-    devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
-
-    enum :role, { admin: 1, user: 0 }
     validates :role, presence: true
 end
